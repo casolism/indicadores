f1115fdc557000b929427b86b1666216fab031f2
Oscar M <omsanchez@improveit.mx>
2014-12-04 11:58:15 -0600

    Implementacion de routing

--- src/angular/app/js/app.js
+++ src/angular/app/js/app.js
@@ -1,3 +1,20 @@
 'use strict';
 
-angular.module('dashboardApp', [])
+angular.module('dashboardApp', [
+	'ui.router'
+])
+.config(function($stateProvider, $urlRouterProvider) {
+	
+	// For unmatch url, we set an default redirect
+	$urlRouterProvider.otherwise("/dashboard");
+
+	$stateProvider
+		.state('dashboard' , {
+			url:'/dashboard',
+			templateUrl: 'partials/dashboard.html'
+		})
+		.state('estadoResultados' , {
+			url:'/estadoResultados',
+			templateUrl: 'partials/estadoResultados.html'
+		})		
+	})
