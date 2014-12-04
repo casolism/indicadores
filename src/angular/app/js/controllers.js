dc17754d8d6a878af8a81620e4f6dc0cf45f40a2
Carlos <ca_solis@hotmail.com>
2014-12-04 13:03:48 -0600

    Ejemplo json para grafica

--- src/angular/app/js/controllers.js
+++ src/angular/app/js/controllers.js
@@ -3,10 +3,17 @@
 /* Controllers */
 
 angular.module('dashboardApp.controllers', [])
-.controller('dashboardCtrl',function($scope){
+.controller('dashboardCtrl',function($scope,$http){
 	$scope.label = "LAPEM"
-	$scope.area = { 
-		'id' : '1',
-		'totalrevenues' : '200', 
-		'totalexpenditure' : '300'}
+	function loadData(){
+		$http
+		.get('http://10.44.6.51/IndicadoresAPI/api/Areas/ff')
+		.success(function(data){
+			console.log(data)
+		})
+		.error(function(){
+			console.log('err')
+		})
+	}
+	loadData()
 })
