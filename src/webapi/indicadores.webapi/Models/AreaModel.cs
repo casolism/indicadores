4975c9ee1d8e305a693ce4559ba2ee2bc929f673
Oscar M <omsanchez@improveit.mx>
2014-12-04 13:11:52 -0600

    Implements GraphValue results return

--- src/webapi/indicadores.webapi/Models/AreaModel.cs
+++ src/webapi/indicadores.webapi/Models/AreaModel.cs
@@ -11,9 +11,27 @@ namespace indicadores.webapi.Models
     /// </summary>
     public class AreaModel
     {
-
         public int id { get;set;}
         public double totalrevenues { get; set; }
         public double totalexpenditure { get; set; }
+        public IList<GraphValue> GraphValue { get; set; }
+
+
+        private void LoadMockDataForGraph()
+        {
+            this.GraphValue.Add(new GraphValue(new DateTime(2003, 8, 24), 0.8709));
+            this.GraphValue.Add(new GraphValue(new DateTime(2003, 8, 25), 0.872));
+            this.GraphValue.Add(new GraphValue(new DateTime(2003, 8, 26), 0.8714));
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AreaModel()
+        {
+            this.GraphValue = new List<GraphValue>();
+            LoadMockDataForGraph();
+        }
+
     }
 }
---
+++ src/webapi/indicadores.webapi/Models/GraphValue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace indicadores.webapi.Models
+{
+    public class GraphValue
+    {
+        public DateTime Date { get; set; }
+        public double Value { get; set; }
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public GraphValue()
+        { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="value"></param>
+        public GraphValue(DateTime date, double value)
+        {
+            this.Date = date;
+            this.Value = value;
+        }
+
+    }
+}
