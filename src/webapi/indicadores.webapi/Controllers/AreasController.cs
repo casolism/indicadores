a99b40439e71c2eeab37c4bbca49d4dd7272da4b
Oscar M <omsanchez@improveit.mx>
2014-12-04 10:39:06 -0600

    Create AreasController with Get method

---
+++ src/webapi/indicadores.webapi/Controllers/AreasController.cs
@@ -0,0 +1,25 @@
+﻿using indicadores.webapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace indicadores.webapi.Controllers
+{
+
+    /// <summary>
+    /// Area's API controller
+    /// </summary>
+    public class AreasController : ApiController
+    {
+
+        //GET api/Areas/K3000
+        public AreaModel Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
